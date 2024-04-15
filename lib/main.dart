afbb290d27a6a42cae6585ec4e4f89d0bfdf2512
benyy0101 <benyy0101@gmail.com>
2024-04-15 22:40:00 +0900

    router and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -9,33 +9,45 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Practice App',
       theme: ThemeData(primarySwatch: Colors.red),
-      home: Mypage(),
+      home: FirstPage(),
     );
   }
 }
 
-class Mypage extends StatelessWidget {
+class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-                height: 100, color: Colors.white, child: Text('Container 1')),
-            SizedBox(
-              width: 30.0,
-            ),
-            Container(
-                height: 100, color: Colors.blue, child: Text('Container 2')),
-            SizedBox(
-              height: 30.0,
-            ),
-            Container(
-                height: 100, color: Colors.red, child: Text('Container 3')),
-          ],
+        appBar: AppBar(
+          title: Text('First Page'),
+        ),
+        body: Center(
+            child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SecondPage()));
+          },
+          child: Text('Go to the Second'),
+        )));
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Second Page'),
+        ),
+        body: Center(
+            child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => FirstPage()));
+          },
+          child: Text('Go to the First'),
         )));
   }
 }
